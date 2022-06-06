0b765340b13a0a08ad2f60b6b4eaa850b1da3c74
Nayan P Ingale <97461960+NayanIngale1@users.noreply.github.com>
2022-06-06 15:15:31 +0530

    update: server req

--- Server/src/controllers/product.controller.js
+++ Server/src/controllers/product.controller.js
@@ -4,29 +4,59 @@ const router = express.Router();
 
 router.get("/list", async (req, res) => {
   try {
-    const { brand, color, from, to, discount } = req.query;
+    const brand = req.query;
+    const color = req.query;
+    const from = req.query || 0;
+    const to = req.query;
+    const discount = req.query;
+    const page = req.query || 1;
+    const sort = req.query;
 
-    let data = Product.find().lean().exec();
-
-    if (brand) {
-      data = data.filter((el) => el.brand === brand);
-    }
-    if (color) {
-      data = data.filter((el) => el.color === color);
-    }
-
-    if (from && to) {
-      data = data.filter((el) => el.price >= from && el.price <= to);
-    }
-
-    if (discount) {
-      data.data.filter((el) => el.discount > discount);
+    let price;
+    if (sort) {
+      if (sort === "price_asc") {
+        price = 1;
+      } else if (sort === "price_desc") {
+        price = -1;
+      }
     }
 
-      
-    return res.status(200).send(body);
+    let offset = (page - 1) * 12;
+
+    let data = Product.find({
+      $and: [
+        { brand: brand },
+        { color: color },
+        { price: { $gte: from } },
+        { price: { $lte: to } },
+        { discount: { $lte: discount } },
+      ],
+    })
+      .sort({ price: price })
+      .skip(offset)
+      .limit(12)
+      .lean()
+      .exec();
+
+    let total_page = Math.ceil(data.length / 12);
+
+    return res.status(200).send({ data, total_page });
   } catch (error) {
     console.log("error:", error);
+    return res.status(500).send({ message: "Something went wrong", error });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const data = Product.create(req.body);
+
+    return res
+      .status(201)
+      .send({ message: "product created succeffully", data });
+  } catch (error) {
+    console.log("error:", error);
+    return res.status(500).send({ message: "Something went wrong", error });
   }
 });
 
